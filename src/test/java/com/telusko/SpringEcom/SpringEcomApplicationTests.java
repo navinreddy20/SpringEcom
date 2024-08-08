7b195a3ea82d9fda2afd73e975c66bca67ad237a
navin <navinreddy20@gmail.com>
2024-08-08 10:19:17 +0530

    Initial commit

---
+++ src/test/java/com/telusko/SpringEcom/SpringEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.SpringEcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
